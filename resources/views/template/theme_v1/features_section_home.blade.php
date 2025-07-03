01ff38de601feecb4b45d4bc12935c0cce550f75
aadilhits <amodan@hostitsmart.com>
2025-07-03 12:40:32 +0530

    cliential-design3-7-v2

--- resources/views/template/theme_v1/features_section_home.blade.php
+++ resources/views/template/theme_v1/features_section_home.blade.php
@@ -40,13 +40,138 @@
 </div>
 </section>
 
+
+<section class="w-serv-netw-main head-tb-p-40">
+
+    <div class="container">
+
+        <div class="row align-items-center">
+
+            <div class="col-lg-6">
+
+                <div class="w-serv-netw-img">
+
+                    <img src="./assets/images/Homepage/w-serv-netw-img1.webp" alt="w-serv-netw-img">
+
+                </div>
+
+            </div>
+
+            <div class="col-lg-6">
+
+                <div class="section-heading">
+
+                    <h2 class="text_head">We are based in India, but our servers travel the world for you!</h2>
+
+                    <p>You will experience blazing-fast performance with Host IT Smart's global server network, featuring robust security and seamless connectivity, which helps your business grow locally while reaching global audiences.</p>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</section>
+
+
+<section class="pillr-hstg-serv-main head-tb-p-40">
+
+    <div class="container">
+
+        <div class="row align-items-center">
+
+            <div class="section-heading">
+
+                <div class="text_head text-center">The 3 Pillars Of A Great Hosting Service</div>
+
+            </div>
+
+            <div class="pillr-hstg-flx">
+
+
+
+                <div class="pillr-hstg-img">
+
+                    <img src="./assets/images/Homepage/Quality-Hardware.webp" alt="Quality-Hardware" loading="lazy">
+
+                </div>
+
+                <div class="pillr-hstg-cnt">
+
+                    <h3>Quality Hardware For Powering Your Online Presence</h3>
+
+                    <p>At Host IT Smart, we understand that successful websites rely on powerful hardware, so we invest in top-tier servers and infrastructure to guarantee optimal performance regardless of traffic volume.</p>
+
+                    <p>Our VPS servers, equipped with robust 2x20 Intel Xeon Gold Processor CPUs, ensure your website can effortlessly handle heavy traffic and complex operations. In addition, our shared hosting servers are built on redundant cloud infrastructure, offering an extra layer of reliability and performance.</p>
+
+                </div>
+
+            </div>
+
+            <div class="pillr-hstg-flx">
+
+                <div class="pillr-hstg-cnt">
+
+                    <h3>Robust Hosting Solutions for Smooth Website Operations</h3>
+
+                    <p>At Host IT Smart, we offer robust hosting systems designed to ensure your websites run smoothly without any technical hiccups. We proudly own and fully control our infrastructure for virtual servers. It is built on high-grade devices and equipped with contingencies that leave zero room for errors.</p>
+
+                    <p>We also provide servers and network devices with a manufacturer’s warranty, ensuring peace of mind and quality assurance. For shared hosting, we go the extra mile with automated weekly snapshots of the server, in addition to backups, ensuring you never lose crucial data.</p>
+
+                </div>
+
+                <div class="pillr-hstg-img">
+
+                    <img src="./assets/images/Homepage/Robust-Hosting.webp" alt="Robust-Hosting" loading="lazy">
+
+                </div>
+
+            </div>
+
+            <div class="pillr-hstg-flx">
+
+                <div class="pillr-hstg-img">
+
+                    <img src="./assets/images/Homepage/Experience-Lightning.webp" alt="Experience-Lightning" loading="lazy">
+
+                </div>
+
+                <div class="pillr-hstg-cnt">
+
+                    <h3>Experience Lightning-Fast Connectivity and Low Latency</h3>
+
+                    <p>Our fast connectivity and low latency are not just promises but realities that power your online presence! Our data infrastructure is built for lightning-fast connectivity, ensuring your website loads in the blink of an eye.</p>
+
+                    <p>You will get the benefit from our Class A Internet Service Provider status and high-speed NVMe SSD servers, which maximize the speed and performance of your online project.</p>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</section>
+
+
+
 <section class="client-trust-indust-main head-tb-p-40">
         <div class="container">
             <div class="client-trust-inudst-box">
-            <div class="row justify-content-center align-items-center">
+            <div class="row justify-content-center align-items-center flex-sm-row-reverse
+">
+                <div class="col-lg-6">
+                    <div class="client-trust-indust-cnt">
+                            <h2 class="text_head">Our Awesome Clients</h2>
+                            <p>From passionate entrepreneurs to established enterprises, we have had the privilege of powering their online journeys. Check out some of the amazing clients we are proud to support!</p>
+                    </div>
+                </div>
                 <div class="col-lg-6">
                     <div class="client-trust-indust-img">
-                        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="650" height="789" viewBox="0 0 650 789">
+                        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="650" height="650" viewBox="0 0 650 789">
   <defs>
     <filter id="Subtraction_12" x="245" y="40" width="325" height="303" filterUnits="userSpaceOnUse">
       <feOffset input="SourceAlpha"/>
@@ -476,131 +601,11 @@
 </svg>
                     </div>
                 </div>
-                <div class="col-lg-6">
-                    <div class="client-trust-indust-cnt">
-                            <h2 class="text_head">Trusted by Industry Leaders. We Redefine Success Together</h2>
-                            <p>We’re not just a web hosting company, we’re your friendly tech partner in the online world! Whether you are starting a blog or running an online store, we ensure that your website is fast, secure, and always up and running.</p>
-                    </div>
-                </div>
                 </div>
             </div>
         </div>
 </section>
 
-<section class="w-serv-netw-main head-tb-p-40">
-
-    <div class="container">
-
-        <div class="row align-items-center">
-
-            <div class="col-lg-6">
-
-                <div class="w-serv-netw-img">
-
-                    <img src="./assets/images/Homepage/w-serv-netw-img1.webp" alt="w-serv-netw-img">
-
-                </div>
-
-            </div>
-
-            <div class="col-lg-6">
-
-                <div class="section-heading">
-
-                    <h2 class="text_head">We are based in India, but our servers travel the world for you!</h2>
-
-                    <p>You will experience blazing-fast performance with Host IT Smart's global server network, featuring robust security and seamless connectivity, which helps your business grow locally while reaching global audiences.</p>
-
-                </div>
-
-            </div>
-
-        </div>
-
-    </div>
-
-</section>
-
-
-<section class="pillr-hstg-serv-main head-tb-p-40">
-
-    <div class="container">
-
-        <div class="row align-items-center">
-
-            <div class="section-heading">
-
-                <div class="text_head text-center">The 3 Pillars Of A Great Hosting Service</div>
-
-            </div>
-
-            <div class="pillr-hstg-flx">
-
-
-
-                <div class="pillr-hstg-img">
-
-                    <img src="./assets/images/Homepage/Quality-Hardware.webp" alt="Quality-Hardware" loading="lazy">
-
-                </div>
-
-                <div class="pillr-hstg-cnt">
-
-                    <h3>Quality Hardware For Powering Your Online Presence</h3>
-
-                    <p>At Host IT Smart, we understand that successful websites rely on powerful hardware, so we invest in top-tier servers and infrastructure to guarantee optimal performance regardless of traffic volume.</p>
-
-                    <p>Our VPS servers, equipped with robust 2x20 Intel Xeon Gold Processor CPUs, ensure your website can effortlessly handle heavy traffic and complex operations. In addition, our shared hosting servers are built on redundant cloud infrastructure, offering an extra layer of reliability and performance.</p>
-
-                </div>
-
-            </div>
-
-            <div class="pillr-hstg-flx">
-
-                <div class="pillr-hstg-cnt">
-
-                    <h3>Robust Hosting Solutions for Smooth Website Operations</h3>
-
-                    <p>At Host IT Smart, we offer robust hosting systems designed to ensure your websites run smoothly without any technical hiccups. We proudly own and fully control our infrastructure for virtual servers. It is built on high-grade devices and equipped with contingencies that leave zero room for errors.</p>
-
-                    <p>We also provide servers and network devices with a manufacturer’s warranty, ensuring peace of mind and quality assurance. For shared hosting, we go the extra mile with automated weekly snapshots of the server, in addition to backups, ensuring you never lose crucial data.</p>
-
-                </div>
-
-                <div class="pillr-hstg-img">
-
-                    <img src="./assets/images/Homepage/Robust-Hosting.webp" alt="Robust-Hosting" loading="lazy">
-
-                </div>
-
-            </div>
-
-            <div class="pillr-hstg-flx">
-
-                <div class="pillr-hstg-img">
-
-                    <img src="./assets/images/Homepage/Experience-Lightning.webp" alt="Experience-Lightning" loading="lazy">
-
-                </div>
-
-                <div class="pillr-hstg-cnt">
-
-                    <h3>Experience Lightning-Fast Connectivity and Low Latency</h3>
-
-                    <p>Our fast connectivity and low latency are not just promises but realities that power your online presence! Our data infrastructure is built for lightning-fast connectivity, ensuring your website loads in the blink of an eye.</p>
-
-                    <p>You will get the benefit from our Class A Internet Service Provider status and high-speed NVMe SSD servers, which maximize the speed and performance of your online project.</p>
-
-                </div>
-
-            </div>
-
-        </div>
-
-    </div>
-
-</section>
 
 
 <script>
