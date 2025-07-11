9c30f703597b4efab3bc0da152a60afaf4fede23
aadilhits <amodan@hostitsmart.com>
2025-07-11 13:11:04 +0530

    cpanel

--- resources/views/cpanel-vps-product.blade.php
+++ resources/views/cpanel-vps-product.blade.php
@@ -1,3 +1,19 @@
 @extends('layouts.app')
 @section('content')
+<div class="vps_main {{$ProductBanner->varBannerIconClass}}">
+    @if (isset($bannerData) && !empty($bannerData) && count($bannerData) > 0)
+    	<link rel="stylesheet" href="{{URL::to('/assets/css/full-width-inner-banner.css')}}">
+    	<?php $themeversion = !isset($_SESSION['themepreview']) ? Config::get('Constant.DEFAULT_THEME') : $_SESSION['themepreview']; ?>
+    	@include('template.'.$themeversion.'.banner')
+
+
+
+
+
+
+
+
+
+        
+	@endif
 @endsection
